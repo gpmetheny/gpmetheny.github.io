2c3cd2e4275e50499724c44f57ac721616ddd9b7
Gabbie <gabbie.metheny@gmail.com>
2018-03-06 10:18:43 -0800

    initial commit

---
+++ src/Components/Success.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+class Success extends Component {
+  componentDidMount() {
+    document.querySelectorAll('nav a').forEach(navlink => navlink.classList.remove('active-link'));
+    document.querySelector('nav ul li:nth-child(3) a').classList.add('active-link');
+  }
+
+  render() {
+    return (
+      <div className="pt-3 d-flex align-items-center justify-content-center content-wrapper" id="success">
+        <div className="success-wrapper container d-flex flex-column flex-md-row align-items-center justify-content-center px-md-5">
+          <div>
+            <h1 className="text-center mb-3">contactMe()</h1>
+          </div>
+          <h3>Thanks! I'll be in touch soon!</h3>
+          {/* <form action="https://formspree.io/gabbie.metheny@gmail.com" method="POST">
+            <div className="form-row text-center">
+              <div className="form-group offset-md-4 col-md-4">
+                <input type="text" className="form-control" name="name" placeholder="Name" />
+              </div>
+              <div className="form-group col-md-4">
+                <input type="email" className="form-control" name="_replyto" placeholder="Email" />
+              </div>
+              <div className="form-group offset-md-4 col-md-8">
+                <textarea className="form-control" name="message" placeholder="Say hi..." rows="10" cols="60"></textarea>
+                <input type="hidden" name="_subject" value="Website Contact" />
+                <input type="hidden" name="_next" value={"#success"} />
+                <input type="text" name="_gotcha" style={{display: 'none'}} />
+              </div>
+              <div className="form-group offset-md-4 col-md-8">
+                <input type="submit" className="btn btn-info" value="Submit" />
+              </div>
+            </div>
+          </form> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Success;
