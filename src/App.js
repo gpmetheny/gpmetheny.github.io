a203aa2924e8c359f732590eb6ecb485268d3ac1
Gabbie <gabbie.metheny@gmail.com>
2018-03-06 07:36:13 -0800

    initial commit for react redesign

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from 'react-router-dom';
+import monstera from './img/monstera.svg';
+import codepen from './img/codepen.svg';
+import github from './img/github.svg';
+import linkedin from './img/linkedin.svg';
+import twitter from './img/twitter.svg';
+import Feature from './Components/Feature';
+import Work from './Components/Work';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import './App.css';
+
+class App extends Component {
+componentDidMount() {
+
+  document.querySelector('nav').addEventListener('click', (e) => {
+    if (e.target.className === 'icon-wrap' || e.target.className === 'icon') {
+      document.querySelector('nav').classList.toggle('menuActive');
+    }
+    if (e.target.tagName === 'A') {
+      document.querySelector('nav').classList.remove('menuActive');
+    }
+  });
+  
+}
+
+  render() {
+    return (
+      <HashRouter>
+        <div className="App">
+          <header>
+            <nav>
+              <div className="menu-wrap">
+                <NavLink className="logo-wrap" to="/">
+                  <img src={monstera} alt="site logo" id="avatar" className="pr-2" />
+                  <h3>/^Gab+[iy]e?$/</h3>
+                </NavLink>
+                <div className="icon-wrap">
+                  <span className="icon" id="icon-top"></span>
+                  <span className="icon" id="icon-center"></span>
+                  <span className="icon" id="icon-bottom"></span>
+                </div>
+              </div>
+              <ul>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/work">Work</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+              </ul>
+            </nav>
+          </header>
+          <main>
+            <Route path="/" exact component={Feature} />
+            <Route path="/work" component={Work} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            {/* <About /> */}
+          </main>
+          <footer>
+            <span>&copy; 2018 Gabbie Metheny</span>
+            <ul>
+              <li className="social-icon"><a href="https://www.twitter.com/gpmetheny" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter Icon" /></a></li>
+              <li className="social-icon"><a href="https://www.linkedin.com/in/gpmetheny" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="LinkedIn Logo" /></a></li>
+              <li className="social-icon"><a href="https://www.github.com/gpmetheny" target="_blank" rel="noopener noreferrer"><img src={github} alt="GitHub Logo" /></a></li>
+              <li className="social-icon"><a href="https://www.codepen.io/gpmetheny" target="_blank" rel="noopener noreferrer"><img src={codepen} alt="Codepen Logo" /></a></li>
+            </ul>
+          </footer>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+  componentDidMount() {
+    document.querySelectorAll('nav a').forEach(navlink => navlink.classList.remove('active-link'));
+    document.querySelector('nav ul li:nth-child(3) a').classList.add('active-link');
+  }
+
+  render() {
+    return (
+      <div className="pt-3 d-flex align-items-center justify-content-center content-wrapper" id="contact">
+        <div className="contact-wrapper container d-flex flex-column flex-md-row align-items-center justify-content-center px-md-5">
+          <div>
+            <h1 className="text-center mb-3">contactMe()</h1>
+          </div>
+          <form action="https://formspree.io/you@email.com" method="POST">
+            <div className="form-row text-center">
+              <div className="form-group offset-md-4 col-md-4">
+                <input type="text" className="form-control" name="name" placeholder="Name" />
+              </div>
+              <div className="form-group col-md-4">
+                <input type="email" className="form-control" name="_replyto" placeholder="Email" />
+              </div>
+              <div className="form-group offset-md-4 col-md-8">
+                <textarea className="form-control" name="message" placeholder="Say hi..." rows="10" cols="60"></textarea>
+                <input type="hidden" name="_subject" value="Website Contact" />
+                <input type="hidden" name="_next" value="//site.io/thanks.html" />
+                <input type="text" name="_gotcha" style={{display: 'none'}} />
+              </div>
+              <div className="form-group offset-md-4 col-md-8">
+                <input type="submit" className="btn btn-info" value="Submit" />
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
