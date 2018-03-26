5fb839a98671822c80002f268e49e60f61181e61
Gabbie <gabbie.metheny@gmail.com>
2018-03-25 18:10:00 -0700

    remove triangle background

--- src/Components/Feature.js
+++ src/Components/Feature.js
@@ -8,29 +8,29 @@ class Feature extends Component {
   }
 
   componentDidMount() {
-    const topTriangles = document.querySelectorAll('.top span');
-    const bottomTriangles = document.querySelectorAll('.bottom span');
-    const topColors = [
-      '#222', '#F94E6D', '#aaa', 'transparent', '#FDCAD3', '#F94E6D', 'transparent', 'transparent', 'transparent'
-    ];
-    const bottomColors = [
-      '#222', 'transparent', 'transparent', 'transparent', 'transparent'
-    ];
+    // const topTriangles = document.querySelectorAll('.top span');
+    // const bottomTriangles = document.querySelectorAll('.bottom span');
+    // const topColors = [
+    //   '#222', '#666', '#aaa', 'transparent', '#666', '#aaa', 'transparent', 'transparent', 'transparent'
+    // ];
+    // const bottomColors = [
+    //   '#222', 'transparent', 'transparent', 'transparent', 'transparent'
+    // ];
 
-    (function changeBackground() {
-      let index = 0;
-      for (index=0; index<topTriangles.length; index++) {
-        let randomColorIndex = Math.floor(Math.random() * 9);
-        let randomColor = topColors[randomColorIndex];
-        topTriangles[index].style.borderTopColor = randomColor;
-      }
+    // (function changeBackground() {
+    //   let index = 0;
+    //   for (index=0; index<topTriangles.length; index++) {
+    //     let randomColorIndex = Math.floor(Math.random() * 9);
+    //     let randomColor = topColors[randomColorIndex];
+    //     topTriangles[index].style.borderTopColor = randomColor;
+    //   }
       
-      for (index=0; index<bottomTriangles.length; index++) {
-        let randomColorIndex = Math.floor(Math.random() * 5);
-        let randomColor = bottomColors[randomColorIndex];
-        bottomTriangles[index].style.borderBottomColor = randomColor;
-      }
-    })()
+    //   for (index=0; index<bottomTriangles.length; index++) {
+    //     let randomColorIndex = Math.floor(Math.random() * 5);
+    //     let randomColor = bottomColors[randomColorIndex];
+    //     bottomTriangles[index].style.borderBottomColor = randomColor;
+    //   }
+    // })()
 
     document.querySelectorAll('nav a').forEach(navlink => navlink.classList.remove('active-link'));
     document.querySelector('nav').classList.remove('menuActive');
@@ -39,7 +39,7 @@ class Feature extends Component {
   render() {
     return (
       <div className="jumbotron jumbotron-fluid content-wrapper" id="feature">
-        <div className="group">
+        {/* <div className="group">
           <div className="top">
             <span></span>
             <span></span>
@@ -128,12 +128,12 @@ class Feature extends Component {
             <span></span>
             <span></span>
           </div>
-        </div>
-        <div className="jumbotron jumbotron-fluid text-center" id="welcome">
+        </div> */}
+        <div className="jumbotron jumbotron-fluid d-flex flex-column align-items-center justify-content-center text-center" id="welcome">
           <img src={portfolioLogo} alt="Gabbie Metheny | Full-Stack JavaScript Developer" className="logo" />
           <Link to="/work" className="more">see my work<span> {'>>'}</span></Link>
         </div>
-        <div className="group">
+        {/* <div className="group">
           <div className="top">
             <span></span>
             <span></span>
@@ -222,7 +222,7 @@ class Feature extends Component {
             <span></span>
             <span></span>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
