20f2533efad4c61a3ec91c4a9c3d3649e264d8f2
Gabbie <gabbie.metheny@gmail.com>
2018-10-31 06:54:44 -0700

    remove active style for CONTACT

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -17,7 +17,6 @@ class Error extends Component {
 
     // menu
     document.querySelectorAll('nav a').forEach(navlink => navlink.classList.remove('active-link'));
-    document.querySelector('nav ul li:nth-child(4) a').classList.add('active-link');
     document.querySelector('nav').classList.remove('menuActive');
   }
 
