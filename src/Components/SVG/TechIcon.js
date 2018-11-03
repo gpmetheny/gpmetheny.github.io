850f0366134f9e7a1f45f8d25025cca155a1a020
Gabbie <gabbie.metheny@gmail.com>
2018-11-03 06:45:53 -0700

    refactor TechIcon to allow for circles

--- src/Components/SVG/TechIcon.js
+++ src/Components/SVG/TechIcon.js
@@ -6,6 +6,11 @@ class TechIcon extends Component {
       <div className="tech-wrap" id={this.props.id}>
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" alt={this.props.id}>
           <title>{this.props.id}</title>
+          {this.props.circles ? this.props.circles.map((circle) => {
+            return (
+              <circle cx={circle.cx} cy={circle.cy} r={circle.r} />
+            );
+          }) : null}
           {this.props.paths.map((path) => {
             return (
               <path d={path} />
