ec852e0efda3049700adc6f398acc49bfb96033a
Gabbie <gabbie.metheny@gmail.com>
2018-11-03 07:24:29 -0700

    move svg id to wrapper li

--- src/Components/SVG/SocialIcon.js
+++ src/Components/SVG/SocialIcon.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 class SocialIcon extends Component {
   render() {
     return (
-      <li className="social-icon">
+      <li className="social-icon" id={this.props.id}>
         <a href={this.props.href} target="_blank" rel="noopener noreferrer">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id={this.props.id}>
+          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path fill={this.props.fill} d={this.props.d}/>
           </svg>
         </a>
