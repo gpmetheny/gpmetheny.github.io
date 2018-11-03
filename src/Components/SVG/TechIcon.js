c2e6db9a75edc7356ce989c926dd70292b3bef3d
Gabbie <gabbie.metheny@gmail.com>
2018-11-03 07:16:40 -0700

    refactor TechIcon to make paths and circles optional, add unique keys

--- src/Components/SVG/TechIcon.js
+++ src/Components/SVG/TechIcon.js
@@ -4,19 +4,22 @@ class TechIcon extends Component {
   render() {
     return (
       <div className="tech-wrap" id={this.props.id}>
+        {/* svg */}
         <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" alt={this.props.id}>
+          {/* title */}
           <title>{this.props.id}</title>
-          {this.props.circles ? this.props.circles.map((circle) => {
+          {/* circle */}
+          {this.props.circles ? this.props.circles.map((circle, index) => {
             return (
-              <circle cx={circle.cx} cy={circle.cy} r={circle.r} />
+              <circle cx={circle.cx} cy={circle.cy} r={circle.r} key={`circle-${index}`} />
             );
           }) : null}
-          {this.props.paths.map((path) => {
+          {/* path */}
+          {this.props.paths ? this.props.paths.map((path, index) => {
             return (
-              <path d={path} />
+              <path d={path.d} key={`path-${index}`} />
             );
-          })}
-          {/* <path d={this.props.d} /> */}
+          }) : null}
         </svg>
       </div>
     );
