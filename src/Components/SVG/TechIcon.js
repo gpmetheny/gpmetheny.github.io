121f34ea4fb46a3a64e25d28c22ad62781fdffd6
Gabbie <gabbie.metheny@gmail.com>
2018-11-03 06:36:10 -0700

    initial commit

---
+++ src/Components/SVG/TechIcon.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class TechIcon extends Component {
+  render() {
+    return (
+      <div className="tech-wrap" id={this.props.id}>
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" alt={this.props.id}>
+          <title>{this.props.id}</title>
+          {this.props.paths.map((path) => {
+            return (
+              <path d={path} />
+            );
+          })}
+          {/* <path d={this.props.d} /> */}
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default TechIcon;
