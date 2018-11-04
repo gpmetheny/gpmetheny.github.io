c3da5674389eccf1b6aa022b29b781caa39c4cd8
Gabbie <gabbie.metheny@gmail.com>
2018-11-04 05:57:34 -0800

    componentize SVG

--- src/Components/SocialIcon.js
+++ src/Components/SocialIcon.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
+import SVG from './SVG';
 
 class SocialIcon extends Component {
   render() {
     return (
       <li className="social-icon" id={this.props.id}>
         <a href={this.props.href} target="_blank" rel="noopener noreferrer">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <SVG
+            id={this.props.id}
+            circles={this.props.circles}
+            paths={this.props.paths}
+          />
+          {/* <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path fill={this.props.fill} d={this.props.d}/>
-          </svg>
+          </svg> */}
         </a>
       </li>
     );
