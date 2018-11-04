9a50819ba97f5f60e726307940d5f26d8b6ab11a
Gabbie <gabbie.metheny@gmail.com>
2018-11-04 05:46:39 -0800

    componentize SVG

--- src/Components/TechIcon.js
+++ src/Components/TechIcon.js
@@ -1,26 +1,15 @@
 import React, {Component} from 'react';
+import SVG from './SVG';
 
 class TechIcon extends Component {
   render() {
     return (
       <div className="tech-wrap" id={this.props.id}>
-        {/* svg */}
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" alt={this.props.id}>
-          {/* title */}
-          <title>{this.props.id}</title>
-          {/* circle */}
-          {this.props.circles ? this.props.circles.map((circle, index) => {
-            return (
-              <circle cx={circle.cx} cy={circle.cy} r={circle.r} key={`circle-${index}`} />
-            );
-          }) : null}
-          {/* path */}
-          {this.props.paths ? this.props.paths.map((path, index) => {
-            return (
-              <path d={path.d} key={`path-${index}`} />
-            );
-          }) : null}
-        </svg>
+        <SVG
+          id={this.props.id}
+          circles={this.props.circles}
+          paths={this.props.paths}
+        />
       </div>
     );
   }
