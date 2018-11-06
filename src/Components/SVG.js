8e8bc970cfd0a4cf5262715c47a3eb7167397062
Gabbie <gabbie.metheny@gmail.com>
2018-11-06 06:32:45 -0800

    add proptypes

--- src/Components/SVG.js
+++ src/Components/SVG.js
@@ -1,14 +1,38 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class SVG extends Component {
-  static defaultProps = { viewBox: "0 0 24 24" };
+  static defaultProps = {
+    viewBox: "0 0 24 24",
+    xmlns: "http://www.w3.org/2000/svg"
+  };
+
+  static propTypes = {
+    viewBox: PropTypes.string.isRequired,
+    height: PropTypes.string,
+    xmlns: PropTypes.string.isRequired,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    id: PropTypes.string,
+    title: PropTypes.string,
+    circles: PropTypes.arrayOf(PropTypes.shape({
+      fill: PropTypes.string,
+      cx: PropTypes.string.isRequired,
+      cy: PropTypes.string.isRequired,
+      r: PropTypes.string.isRequired
+    })),
+    paths: PropTypes.arrayOf(PropTypes.shape({
+      fill: PropTypes.string,
+      d: PropTypes.string.isRequired
+    }))
+  };
 
   render() {
     return (
       <svg
         viewBox={this.props.viewBox}
         height={this.props.height}
-        xmlns="http://www.w3.org/2000/svg"
+        xmlns={this.props.xmlns}
         alt={this.props.alt}
         className={this.props.className}
         id={this.props.id}
