182ae632544c1283f1b90f5d0b0f139cf0340665
Gabbie <gabbie.metheny@gmail.com>
2019-01-14 06:39:55 -0800

    convert Loader to styled component

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,15 +1,70 @@
 import React, {Component} from 'react';
+import styled, {keyframes} from 'styled-components';
 
-import '../Loading.css';
+const pulse = keyframes`
+  20% {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  to {
+    opacity: 0;
+    z-index: -1000;
+  }
+`;
+
+const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: calc(50% - 2.5rem);
+  bottom: calc(50% - 2.5rem);
+  left: calc(50% - 7.5rem);
+  right: calc(50% - 7.5rem);
+  z-index: 1000;
+  animation: ${fadeOut} .25s 2.5s ease-in forwards;
+`;
+
+const Dot = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: .5rem;
+  background: #fff;
+
+  &,
+  &::after {
+    height: 1.5rem;
+    width: 1.5rem;
+    border-radius: 50%;
+  }
+
+  &::after {
+    content: '';
+    background: #F94E6D;
+    opacity: .5;
+    animation: ${pulse} .6s ease-in-out 5 both;
+  }
+
+  &:nth-child(2)::after {
+    animation-delay: .15s;
+  }
+
+  &:nth-child(3)::after {
+    animation-delay: .3s;
+  }
+`;
 
 class Loader extends Component {
   render() {
     return (
-      <div className="loading">
-        <div className="circle"></div>
-        <div className="circle"></div>
-        <div className="circle"></div>
-      </div>
+      <Loading>
+        <Dot />
+        <Dot />
+        <Dot />
+      </Loading>
     );
   }
 }
