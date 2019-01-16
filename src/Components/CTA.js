6a440104c546f0a95d7ed7232b59a14921e3b9d4
Gabbie <gabbie.metheny@gmail.com>
2019-01-16 07:27:40 -0800

    style CTA

--- src/Components/CTA.js
+++ src/Components/CTA.js
@@ -2,10 +2,12 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const CTA = styled(Link)`
+  display: inline-block;
+  min-width: 16rem;
   font-size: 1.25rem;
-  padding: .5rem 1rem;
+  padding: .75rem 2rem;
   background-color: ${props => props.theme.color.brand};
-  border: 3px solid ${props => props.theme.color.black};
+  border-radius: 2px;
   text-decoration: none !important;
 
   &:hover,
