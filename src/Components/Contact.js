37cbaaf179ba9c972167999c4c5f91e84b331e01
Gabbie <gabbie.metheny@gmail.com>
2019-01-24 06:46:12 -0800

    refactor form listener

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -22,19 +22,23 @@ class _Contact extends Component {
 
     // form listener
     document.querySelector('form').addEventListener('keyup', (event) => {
+      // should only fire on required form fields
+      // prevents focus bug when navigating w/ keyboard
       if (event.target.classList.contains('required')) {
-        let count=0;
-        document.querySelectorAll('.required').forEach(field => {
-          if (field.value !== '') {
+        
+        const requiredFields = Array.from(document.querySelectorAll('.required'));
+        const submitButton = document.querySelector('#submit');
+
+        requiredFields.reduce((count, field) => {
+          if (field.value) {
             count += 1;
-            if (count >= document.querySelectorAll('.required').length) {
-              document.querySelector('#submit').disabled = false;
-              return;
-            } else {
-              document.querySelector('#submit').disabled = true;
-            }
+            count >= requiredFields.length
+              ? submitButton.disabled = false
+              : submitButton.disabled = true;
+            return count;
           }
-        });
+          return null;
+        }, 0);
       }
     });
   }
